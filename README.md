d57f8b2ba6bea0b0e85f127e3dd04c018d633a3f
Vyacheslav Aprosinckin <vyacheslav.aprosinckin@yandex.ru>
2021-06-12 18:11:54 +0300

    add Test fun createComment

--- README.md
+++ README.md
@@ -12,4 +12,5 @@
 
 
 # 3.1. Исключения #
-##Задача №1 - PostNotFoundException ##
+## Задача №1 - PostNotFoundException ##
+## Задача №2 - JUnit ##
--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -15,8 +15,8 @@ class MainKtTest {
 
     @Test
     fun update_true() {
-        val post1 = Post(id=11)
-        val post2 = Post(id=0, text = "NEW TEXT")
+        val post1 = Post(id = 11)
+        val post2 = Post(id = 0, text = "NEW TEXT")
         Wallservice.add(post1)
         val result = Wallservice.update(post2)
         assertTrue(result)
@@ -32,4 +32,26 @@ class MainKtTest {
         assertFalse(result)
 
     }
+
+    @Test
+    fun shouldAddComment() {
+        val postForComment1 = Post(1)
+        val commentForTest1 = Comment(100, "Комментарий добавлен!")
+        val expected = "Комментарий добавлен!"
+
+        Wallservice.add(postForComment1)
+        Wallservice.createComment(commentForTest1, 0)
+
+        val result = commentForTest1.text
+
+        assertEquals(expected, result)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun shouldThrow() {
+        val commentForTest2 = Comment(55, "Проверка!")
+        Wallservice.createComment(commentForTest2, 42)
+    }
+
+
 }
